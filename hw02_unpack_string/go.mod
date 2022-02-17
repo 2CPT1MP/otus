87dfe50e93095d03192bfaf9f1b9d9d66b185484
Иваненко Владислав Игоревич <vladislav.ivanenko@cinimex.ru>
2022-02-17 21:50:56 +0300

    HW 2

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw02_unpack_string
+module github.com/2cpt1mp/otus/hw02_unpack_string
 
 go 1.16
 
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,56 @@
-package hw02unpackstring
+package main
 
 import (
 	"errors"
+	"fmt"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+
+	var stringBuilder strings.Builder
+	var previousChar rune
+
+	for _, char := range str {
+
+		if !unicode.IsDigit(char) {
+
+			if previousChar == 0 {
+				previousChar = char
+			} else {
+				stringBuilder.WriteRune(previousChar)
+				previousChar = char
+			}
+
+		} else if previousChar != 0 {
+			repeatedChar := strings.Repeat(string(previousChar), int(char-'0'))
+			stringBuilder.WriteString(repeatedChar)
+
+			previousChar = 0
+		} else {
+			return "", ErrInvalidString
+		}
+
+	}
+
+	if previousChar != 0 {
+		stringBuilder.WriteRune(previousChar)
+	}
+
+	return stringBuilder.String(), nil
+}
+
+func main() {
+	fmt.Println(Unpack("a4bc2d5e"))
+	fmt.Println(Unpack("abcd"))
+	fmt.Println(Unpack("3abc"))
+	fmt.Println(Unpack("45"))
+	fmt.Println(Unpack("aaa10b"))
+	fmt.Println(Unpack("aaa0b"))
+	fmt.Println(Unpack(""))
+	fmt.Println(Unpack("d\n5abc"))
+
 }
